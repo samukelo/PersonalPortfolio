747c8daa747e32a7420ba221cdd42e649fc3d3e3
Samukaay <Samukelomakofane@gmail.com>
2024-09-17 10:57:14 +0200

    upda

--- src/components/projectssection.tsx
+++ src/components/projectssection.tsx
@@ -1,9 +1,11 @@
 import web1 from "../../public/web1.png";
 import web2 from "../../public/drone.png";
+import web3 from "../../public/animezone.png";
 import github from "../../public/github.svg";
 import link from "../../public/link.svg";
 function ProjectSection() {
   return (
+    <>
     <div id="projects" className="projects-section">
       <div className="container">
         <h2>My Projects</h2>
@@ -63,9 +65,39 @@ function ProjectSection() {
               </ul>
             </p>
           </div>
+
+            <div className="project-card">
+              <img src={web3} alt="Project 2" />
+              <h3>Anime Zone Movie Site</h3>
+              <p>A simple Movie And Series Site That Saves Data to  a certain folder / file (.json) But I Used State</p>
+              <p>
+                <ul className="icons">
+                  <li>
+                    <a
+                      href="https://movie-series-site.vercel.app/"
+                      target="_blank"
+                      className="icon Linkedin"
+                    >
+                      <img src={link} alt="" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://github.com/samukelo/firstreact.git"
+                      target="_blank"
+                      className="icon Github"
+                    >
+                      <img src={github} alt="" />
+                    </a>
+                  </li>
+                </ul>
+              </p>
+            </div>
+
         </div>
       </div>
     </div>
+    </>
   );
 }
 
